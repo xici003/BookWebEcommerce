003fad16231afcba9c45ba9a755063781f14d927
xici003 <nqchi2003@gmail.com>
2023-10-30 17:23:24 +0700

    Edit_L5

--- ViewModel/RegisterVM.cs
+++ ViewModel/RegisterVM.cs
@@ -6,10 +6,12 @@ namespace BookWebEcommerce.ViewModel
     {
         [Display(Name = "Full name")]
         [Required(ErrorMessage = "Full name is required")]
+        [StringLength(20, MinimumLength = 4)]
         public string FullName { get; set; }
 
         [Display(Name = "Email address")]
         [Required(ErrorMessage = "Email address is required")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@gmail.com", ErrorMessage = "Email must be gmail.com")]
         public string EmailAddress { get; set; }
 
         [Required]
